4382ac0eee5c1df3b1f914c9dae12807956b664c
Bruno  Cota <brunocota@MacBook-Air-de-Bruno.local>
2022-07-22 00:30:18 -0300

    ajuste arquivos staticos

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('/public'))
+app.use(express.static('views'))
+app.set('view engine', 'ejs');
 
+app.get('/',(request,response)=>{
+    response.render("index.ejs")
+})
 
 app.listen(3000, () => {
     console.log('Servidor rodando')
